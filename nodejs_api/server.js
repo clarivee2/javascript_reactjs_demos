077778fa2b8fa224395da83f4bc4c8af3d21fa42
Chris Larivee <christian.larivee@gmail.com>
2018-02-17 21:14:21 -0400

    Full working sample

--- nodejs_api/server.js
+++ nodejs_api/server.js
@@ -2,12 +2,71 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
+// ******************************************
+//   list users
+// ******************************************
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
 })
+
+// ******************************************
+//   add a user
+// ******************************************
+// TODO - accept input parms instead
+var user = {
+    "user4" : {
+       "name" : "mohit",
+       "password" : "password4",
+       "profession" : "teacher",
+       "id": 4
+    }
+ }
+ 
+ app.post('/addUser', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        data["user4"] = user["user4"];
+        console.log( data );
+        res.end( JSON.stringify(data));
+    });
+ })
+ 
+// ******************************************
+//   fetch a user by id
+// ******************************************
+ app.get('/:id', function (req, res) {
+    // First read existing users.
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       var users = JSON.parse( data );
+       var user = users["user" + req.params.id] 
+       console.log( user );
+       res.end( JSON.stringify(user));
+    });
+ })
+ 
+// ******************************************
+//   delete a user by id
+// ******************************************
+// TODO - Make take a parm
+//TODO - Make persistent
+ var id = 2;
+
+app.delete('/deleteUser', function (req, res) {
+
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+       data = JSON.parse( data );
+       delete data["user" + 2];
+       
+       console.log( data );
+       res.end( JSON.stringify(data));
+   });
+})
+
 
 var server = app.listen(8085, function () {
 
