baf5a1063d1cb3e90c7d9e530f193cb42d5fb72a
Chris Larivee <christian.larivee@gmail.com>
2018-02-19 11:40:28 -0400

    Add second get endpoint as a test

--- nodejs_api/server.js
+++ nodejs_api/server.js
@@ -12,6 +12,13 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
+app.get('/listUsers2', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+ })
+
 // ******************************************
 //   add a user
 // ******************************************
